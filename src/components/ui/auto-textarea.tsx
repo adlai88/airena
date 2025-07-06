0675078c451beeaba574eeb8500fa2045e57a7eb
adlai88 <adlai88@gmail.com>
2025-07-06 17:18:15 +0800

    Fix typo in import statement
    Removed an accidental 'h' character at the start of the React import statement in auto-textarea.tsx.

--- src/components/ui/auto-textarea.tsx
+++ src/components/ui/auto-textarea.tsx
@@ -1,4 +1,4 @@
-import * as React from "react";
+himport * as React from "react";
 import { cn } from "@/lib/utils";
 
 export type AutoTextareaProps = React.TextareaHTMLAttributes<HTMLTextAreaElement> & {
